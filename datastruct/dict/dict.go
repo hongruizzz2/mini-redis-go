e89a9d0339a1500080e80859f2c50e01a94902b5
hongrui <1169948065@qq.com>
2024-10-21 23:03:15 +0800

    feat: redis底层数据结构完成

---
+++ datastruct/dict/dict.go
@@ -0,0 +1,19 @@
+package dict
+
+type Consumer func(key string, val interface{}) bool
+
+type Dict interface {
+	Get(key string) (val interface{}, exists bool)
+	Len() int
+	// SET
+	Put(key string, val interface{}) (result int)
+	// SETNX
+	PutIfAbsent(key string, val interface{}) (result int)
+	PutIfExists(key string, val interface{}) (result int)
+	Remove(key string) (result int)
+	ForEach(consumer Consumer)
+	Keys() []string
+	RandomKeys(limit int) []string
+	RandomDistinctKeys(limit int) []string
+	clear()
+}
