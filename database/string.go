63d8292cd4ddd600cddddac03f90357ec702a0bc
hongrui <1169948065@qq.com>
2024-10-22 01:38:34 +0800

    feat: 实现STRING命令集

---
+++ database/string.go
@@ -0,0 +1,71 @@
+package database
+
+import (
+	"mini-redis-go/interface/Database"
+	"mini-redis-go/interface/resp"
+	"mini-redis-go/resp/reply"
+)
+
+// GET
+func execGet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+	bytes := entity.Data.([]byte)
+	return reply.MakeBulkReply(bytes)
+}
+
+// SET k1 v1
+func execSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &Database.DataEntity{
+		Data: value,
+	}
+	db.PutEntity(key, entity)
+	return reply.MakeOkReply()
+}
+
+// SETNX
+func execSetnx(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &Database.DataEntity{
+		Data: value,
+	}
+	result := db.PutIfAbsent(key, entity)
+	return reply.MakeIntReply(int64(result))
+}
+
+// GETSET k1 v1
+func execGetSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity, exists := db.GetEntity(key)
+	db.PutEntity(key, &Database.DataEntity{Data: value})
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+	return reply.MakeBulkReply(entity.Data.([]byte))
+}
+
+// STRLEN
+func execStrLen(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+	bytes := entity.Data.([]byte)
+	return reply.MakeIntReply(int64(len(bytes)))
+}
+
+func init() {
+	RegisterCommand("Get", execGet, 2)
+	RegisterCommand("Set", execSet, -3)
+	RegisterCommand("SetNx", execSetnx, 3)
+	RegisterCommand("GetSet", execGetSet, 3)
+	RegisterCommand("StrLen", execStrLen, 2)
+}
